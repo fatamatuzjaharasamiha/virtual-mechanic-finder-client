3673025bd9f75b9d21c24bb0d65d4f93ef9360b7
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-12-24 15:46:30 +0600

    initial navbar setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Samiha</h1>
+    <div className="App font-[poppins]">
+      <Home></Home>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,31 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#022278",
+
+          "secondary": "#FFB600",
+
+          "accent": "#F8F5FF",
+
+          "neutral": "#3D4451",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
